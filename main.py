08d122a6df3441680a8a01786f8d8db00863aaae
amirul-ic <amirulshafiq.dc@gmail.com>
2023-11-10 09:43:18 +0800

    added first draft

--- main.py
+++ main.py
@@ -25,3 +25,25 @@ def load_data():
         return index
 
 index = load_data()
+
+
+chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+
+
+if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages: # Display the prior chat messages
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+
+
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = chat_engine.chat(prompt)
+            st.write(response.response)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message) # Add response to message history
