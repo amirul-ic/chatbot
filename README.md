1bcd7b501efcbb07273f5ba3410a8743a5d575f6
amirul-ic <amirulshafiq.dc@gmail.com>
2023-11-10 17:47:53 +0800

    revert to original structure

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 Link to the Streamlit app : https://faq-chatbot.streamlit.app/
 
 
-This app utilize the documents in FAQ section in Unifi website:
+This app chat with the Unifi Home FAQ document retrieved from the website:
 https://unifi.com.my/sites/default/files/html/List-FAQ/Home/General-FAQ-unifi-Home-Plan.pdf
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from llama_index.llms import OpenAI
 import openai
 from llama_index import SimpleDirectoryReader 
 # from llama_index import download_loader
-from llama_hub.web.async_web import AsyncWebPageReader
+# from llama_hub.web.async_web import AsyncWebPageReader
 
 st.set_page_config(page_title="Chat with the Unifi Home docs", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets.openai_key
@@ -18,17 +18,17 @@ if "messages" not in st.session_state.keys(): # Initialize the chat messages his
 
 @st.cache_resource(show_spinner=False)
 def load_data():
-    with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
-        # reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
-        # docs = reader.load_data()
+    with st.spinner(text="Loading and indexing the FAQ docs – hang tight! This should take 1-2 minutes."):
+        reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+        docs = reader.load_data()
 
         # UnstructuredURLLoader = download_loader("UnstructuredURLLoader", custom_path=".")
         # urls = ["https://unifi.com.my/support/faq"]
         # loader = UnstructuredURLLoader(urls=urls, continue_on_failure=False, headers={"User-Agent": "value"})
         # docs = loader.load()
 
-        loader = AsyncWebPageReader()
-        docs = loader.load_data(urls=['https://unifi.com.my/support/faq'])
+        # loader = AsyncWebPageReader()
+        # docs = loader.load_data(urls=['https://unifi.com.my/support/faq'])
 
         service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
