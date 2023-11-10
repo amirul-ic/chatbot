1901d954c55bebc66ed6c777a023664f32e0fe64
amirul-ic <amirulshafiq.dc@gmail.com>
2023-11-10 10:29:45 +0800

    updated m script

--- main.py
+++ main.py
@@ -4,16 +4,15 @@ from llama_index.llms import OpenAI
 import openai
 from llama_index import SimpleDirectoryReader
 
+st.set_page_config(page_title="Chat with the Streamlit docs", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets.openai_key
 
 st.header("Chat with the Streamlit docs 💬 📚")
-
-if "messages" not in st.session_state.keys(): # Initialize the chat message history
+         
+if "messages" not in st.session_state.keys(): # Initialize the chat messages history
     st.session_state.messages = [
         {"role": "assistant", "content": "Ask me a question about Streamlit's open-source Python library!"}
     ]
-
-
 
 @st.cache_resource(show_spinner=False)
 def load_data():
@@ -26,9 +25,8 @@ def load_data():
 
 index = load_data()
 
-
-chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
-
+if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
+        st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
 
 if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
@@ -37,13 +35,11 @@ for message in st.session_state.messages: # Display the prior chat messages
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
-
-
 # If last message is not from assistant, generate a new response
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            response = chat_engine.chat(prompt)
+            response = st.session_state.chat_engine.chat(prompt)
             st.write(response.response)
             message = {"role": "assistant", "content": response.response}
             st.session_state.messages.append(message) # Add response to message history
